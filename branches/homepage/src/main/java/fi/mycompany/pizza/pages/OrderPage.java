d7aaf88cfa646321b2d8ce83a6ce70c857fbf91a
toni.paloniemi <toni.paloniemi@4749460c-1c7b-11de-9433-ad2e3fad96c5>
2009-04-01 11:52:45 +0000

    A branch for adding the home page to the software, which shows open orders, history of orders and link to debts-page

---
+++ branches/homepage/src/main/java/fi/mycompany/pizza/pages/OrderPage.java
@@ -0,0 +1,131 @@
+package fi.mycompany.pizza.pages;
+
+import fi.mycompany.pizza.*;
+import fi.mycompany.pizza.model.Order;
+import fi.mycompany.pizza.model.OrderRow;
+import java.util.LinkedList;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.ajax.AbstractAjaxTimerBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.AjaxSelfUpdatingTimerBehavior;
+import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.form.StatelessForm;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.time.Duration;
+
+/**
+ * Homepage
+ */
+public class OrderPage extends WebPage {
+
+	private static final long serialVersionUID = 1L;
+
+	// TODO Add any page properties or variables here
+
+    protected Order getOrder(){
+        return PizzaApplication.getOrder();
+    }
+
+    /**
+	 * Constructor that is invoked when page is invoked without a session.
+	 * 
+	 * @param parameters
+	 *            Page parameters
+	 */
+    public OrderPage(final PageParameters parameters) {
+        //final PropertyModel<List<OrderRow>> model = new PropertyModel<List<OrderRow>>(PizzaApplication.getOrder(), "rows");
+        final LoadableDetachableModel model = new LoadableDetachableModel() {
+
+            @Override
+            protected Object load() {
+                return getOrder().getRows();
+            }
+        };
+        final WebMarkupContainer tableContainer = new WebMarkupContainer("container");
+        final ListView<OrderRow> rowView = new ListView<OrderRow>("rows", model) {
+
+            @Override
+            protected void populateItem(ListItem<OrderRow> item) {
+                final OrderRow orderRow = item.getModelObject();
+                item.add(new Label("orderer", new PropertyModel<OrderRow>(orderRow, "orderer")));
+                item.add(new Label("content", new PropertyModel<OrderRow>(orderRow, "content")));
+                item.add(new Label("price", new PropertyModel<OrderRow>(orderRow, "price")));
+                item.add(new AjaxFallbackLink("removeLink") {
+
+                    @Override
+                    public void onClick(AjaxRequestTarget target) {
+                        target.addComponent(tableContainer);
+                        synchronized(getOrder()){
+                            getOrder().getRows().remove(orderRow);
+                        }
+                        
+                    }
+                });
+            }
+
+        };
+        tableContainer.setOutputMarkupId(true);
+        tableContainer.add(rowView);
+        tableContainer.add(new Label("total", new AbstractReadOnlyModel<Double>() {
+
+            @Override
+            public Double getObject() {
+                double total = 0;
+                synchronized(PizzaApplication.getOrder()){
+                    for (OrderRow orderRow : getOrder().getRows()) {
+                        total += orderRow.getPrice();
+                    }
+                }
+                return total;
+            }
+        }));
+        tableContainer.add(new AjaxSelfUpdatingTimerBehavior(Duration.seconds(2)));
+
+        add(tableContainer);
+        add(new OrderRowForm("newRow"));
+        add(new Chat("chat") {
+
+            @Override
+            protected LinkedList<Message> getMessageList() {
+                return PizzaApplication.getMessageList();
+            }
+        });
+        // TODO Add your page's components here
+    }
+    
+
+    public static class OrderRowForm extends StatelessForm<OrderRow> {
+
+        public OrderRowForm(String id) {
+            super(id);
+            OrderRow row = new OrderRow();
+            setModel(new CompoundPropertyModel<OrderRow>(row));
+            add(new RequiredTextField("orderer"));
+            add(new RequiredTextField("content"));
+            add(new RequiredTextField("price"));
+            
+        }
+        @Override
+        protected void onSubmit() {
+            OrderRow row = getModelObject();
+            synchronized(PizzaApplication.getOrder()){
+                PizzaApplication.getOrder().getRows().add(row);
+            }
+            
+            setModel(new CompoundPropertyModel<OrderRow>(new OrderRow()));
+            setResponsePage(OrderPage.class);
+        }
+
+
+
+    }
+}
---
+++ branches/homepage/src/test/java/com/mycompany/PizzaApp/TestHomePage.java
@@ -0,0 +1,32 @@
+package com.mycompany.PizzaApp;
+
+import fi.mycompany.pizza.pages.OrderPage;
+import fi.mycompany.pizza.PizzaApplication;
+import junit.framework.TestCase;
+import org.apache.wicket.util.tester.WicketTester;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestHomePage extends TestCase
+{
+	private WicketTester tester;
+
+	@Override
+	public void setUp()
+	{
+		tester = new WicketTester(new PizzaApplication());
+	}
+
+	public void testRenderMyPage()
+	{
+		//start and render the test page
+		tester.startPage(OrderPage.class);
+
+		//assert rendered page class
+		tester.assertRenderedPage(OrderPage.class);
+
+		//assert rendered label component
+//		tester.assertLabel("message", "If you see this message wicket is properly configured and running");
+	}
+}
